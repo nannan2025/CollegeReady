b1cb4913fe70ad3c0826e2626bbe1af580639e75
Nan Lin Wah Sin Htoo (Nan Nan) <121724887+nannan2025@users.noreply.github.com>
2024-04-24 17:58:41 -0400

    Using reactDOM.render

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
-// import ReactDOM from "react-dom";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-// import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
 import { createRoot } from "react-dom/client";
 
 const domNode = document.getElementById("root");
@@ -14,16 +14,13 @@ root.render(
   </BrowserRouter>
 );
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
+ReactDOM.render(
+   <React.StrictMode>
+     <BrowserRouter>
+       <App />
+     </BrowserRouter>
+   </React.StrictMode>,
+   document.getElementById("root")
+);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+
